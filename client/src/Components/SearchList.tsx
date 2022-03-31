c8c63ab407ab1f169887668ab1a83506735ad9d0
kimdaeyoon <eodbszla@gmail.com>
2022-03-31 13:03:30 +0900

    Fix: 비동기 오류 수정

--- client/src/Components/SearchList.tsx
+++ client/src/Components/SearchList.tsx
@@ -1,16 +1,26 @@
 import { nanoid } from 'nanoid';
 import styled from 'styled-components';
 
+// interface ISearchListProps {
+//   list: string[];
+//   chooseList: any;
+// }
+
 const List = styled.li`
   width: 100%;
   height: 100%;
   cursor: pointer;
   margin-bottom: 10px;
   background-color: ${(props) => props.theme.beige};
+  transition: all 0.7s;
+  &:hover {
+    background-color: ${(props) => props.theme.green};
+    padding: 3px 0px;
+  }
 `;
 
 function SearchList({ list, chooseList }: any) {
-  return list.map((el: string[]) => (
+  return list.map((el: string) => (
     <List key={nanoid()} value={el} onClick={chooseList}>
       {el}
     </List>
